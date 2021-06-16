896dac45e57eef2f1f84e0660c18be1f908d92e1
slagrabos <sla.grabos@gmail.com>
2021-06-16 12:08:36 +0200

    Add project files.

---
+++ MeetupProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MeetupAPINew.Entities;
+using MeetupAPINew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetupAPINew
+{
+ public class MeetupProfile : Profile
+ {
+  public MeetupProfile()
+  {
+   CreateMap<Meetup, MeetupDetailsDto>()
+       .ForMember(m => m.City, map => map.MapFrom(meetup => meetup.Location.City))
+       .ForMember(m => m.PostCode, map => map.MapFrom(meetup => meetup.Location.PostCode))
+       .ForMember(m => m.Street, map => map.MapFrom(meetup => meetup.Location.Street));
+
+   CreateMap<MeetupDto, Meetup>();
+
+   CreateMap<LectureDto, Lecture>()
+       .ReverseMap();
+  }
+ }
+}
---
+++ MeetupSeeder.cs
@@ -0,0 +1,84 @@
+﻿using MeetupAPINew.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetupAPINew
+{
+ public class MeetupSeeder
+ {
+  private readonly MeetupContext _meetupContext;
+
+  public MeetupSeeder(MeetupContext meetupContext)
+  {
+   _meetupContext = meetupContext;
+  }
+
+  public void Seed()
+  {
+   if (_meetupContext.Database.CanConnect())
+   {
+    if (!_meetupContext.Meetups.Any())
+    {
+     InsertSampleData();
+    }
+   }
+  }
+
+  private void InsertSampleData()
+  {
+   var meetups = new List<Meetup>
+            {
+                new Meetup
+                {
+                    Name = "C# Meeting",
+                    Date = DateTime.Now.AddDays(7),
+                    IsPrivate = false,
+                    Organizer = "Microsoft",
+                    Location = new Location
+                    {
+                        City = "Berlin",
+                        Street = "Sonnenstraße 33",
+                        PostCode = "10001"
+                    },
+                    Lectures = new List<Lecture>
+                    {
+                        new Lecture
+                        {
+                            Author = "Mads Torgersen",
+                            Topic = "C# 10",
+                            Description = "New things in C# 10"
+                        }
+                    }
+                },
+                new Meetup
+                {
+                    Name = ".NET Meeting",
+                    Date = DateTime.Now.AddDays(7),
+                    IsPrivate = false,
+                    Organizer = "Microsoft",
+                    Location = new Location
+                    {
+                        City = "London",
+                        Street = "Sun street 4",
+                        PostCode = "8200"
+                    },
+                    Lectures = new List<Lecture>
+                    {
+                        new Lecture
+                        {
+                            Author = "Tilman Börner",
+                            Topic = ".NET 6.0",
+                            Description = "New things in .NET 6.0"
+                        }
+
+                    }
+                },
+            };
+
+   _meetupContext.AddRange(meetups);
+   _meetupContext.SaveChanges();
+  }
+ }
+}
